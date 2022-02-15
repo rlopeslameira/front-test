34eb886b2b762d26a5bc33f576e268852ca0d4c0
Rodrigo Lopes <rlopeslameira@gmail.com>
2022-02-15 18:02:37 -0300

    date pick

--- src/pages/ClienteCad/index.js
+++ src/pages/ClienteCad/index.js
@@ -1,53 +1,46 @@
-import { TextField, Card, CardActions, FormControl, FormGroup, Input, InputLabel, Button } from '@material-ui/core';
+import { Card, CardActions, FormControl, FormGroup, Input, InputLabel, Button } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-import { useForm } from "react-hook-form";
-import {KeyboardDatePicker } from '@material-ui/pickers';
+import { useForm, Controller } from "react-hook-form";
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns'
+import InputMask from 'react-input-mask';
 
 function ClienteCad() {
-    
 
-    const { register, handleSubmit, getValues, setValue } = useForm({
-        defaultValues: {
-            name: '',
-            birthDate: new Date(),
-        }
-    });
-    const [date, setDate] = useState(new Date());
-    const value = getValues('fieldName');
-
-    useEffect(() => {
-        register('fieldName');
-      }, [register]);
-
-      useEffect(() => {
-        setDate(value || null);
-      }, [setDate, value]);
+    const { register, control, handleSubmit } = useForm();
+    const [selectedDate, handleDateChange] = useState(new Date());
 
     const onSubmit = data => console.log(data);
 
     return (
         <Card>
-            <FormGroup style={{padding: 20}} onSubmit={handleSubmit(onSubmit)}>
-                <FormControl>
-                    <InputLabel>Name: </InputLabel>     
-                    <Input {...register('name')} />                    
-                </FormControl>
-                <FormControl style={{marginTop: 10}}>
-                    <InputLabel>Address: </InputLabel>
-                    <KeyboardDatePicker
-                        value={date}
-                        onChange={(date) => setValue('fieldName', date, { shouldValidate: true, shouldDirty: true })}
-                    />
-                </FormControl>
-                <FormControl style={{marginTop: 10}}>
-                    <InputLabel>Address: </InputLabel>
-                    <Input {...register('address')}/>
-                </FormControl>
-            </FormGroup>
-            <CardActions >
-                <Button size="small" color="secondary">Cancelar</Button>
-                <Button size="small" variant="contained" color="primary">Gravar</Button>
-        </CardActions>
+            <form style={{padding: 20}} onSubmit={handleSubmit(onSubmit)}>
+                <FormGroup >
+                    <FormControl>
+                        <InputLabel>Name: </InputLabel>     
+                        <Input {...register('name')} />                    
+                    </FormControl>
+                    <FormControl>
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <Controller name="brithdate" control={control} defaultValue={selectedDate}
+                                render={()=> <DatePicker label="BirthDate" format='dd/MM/yyyy' value={selectedDate} onChange={handleDateChange}/>
+                            }/>                        
+                        </MuiPickersUtilsProvider>  
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel>Address: </InputLabel>
+                        <Input {...register('address')}/>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel>ZipCode: </InputLabel>
+                        <Input {...register('zipcode')}/>
+                    </FormControl>
+                </FormGroup>
+                <CardActions style={{marginTop: 20}}>
+                    <Button size="small" color="secondary">Cancel</Button>
+                    <Button size="small" type='submit' variant="contained" color="primary">Save</Button>
+                </CardActions>
+            </form>
         </Card>
     );
 }
